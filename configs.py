3873a65c53b7c84275f0ee98663a687b6c22f46b
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2024-12-14 19:57:24 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config:
     API_ID = int(getenv("API_ID", "20389440"))
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
     BOT_TOKEN = getenv("BOT_TOKEN", "7367456788:AAEhMD03_XNhoilZNjBAUuv4ICI4-ddkyhU")
-    FSUB = getenv("FSUB", "VJ_Botz")
+    FSUB = getenv("FSUB", "Aerobots_tm")
     CHID = int(getenv("CHID", "-1002440247365"))
     SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
